bbe7c5133c736de3e9ace9650f88bf94fc95a9a2
Romulo Rocha <romulorocha063@gmail.com>
2019-09-06 15:25:23 -0300

    Add reactotron

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import Header from './components/Header';
 import GlobalStyle from './styles/global';
+import './config/ReactotronConfig';
 
 import store from './store';
 
