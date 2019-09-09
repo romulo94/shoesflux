ee1628f818001097cbb382180bba0f6acda389a2
Romulo Rocha <romulorocha063@gmail.com>
2019-09-09 09:10:09 -0300

    No duplicate product in cart

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   plugins: ['react', 'prettier'],
   rules: {
     'prettier/prettier': 'error',
+    'no-param-reassign': 'off',
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'no-console': ['error', { allow: ['tron'] }],
