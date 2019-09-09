9255ff6f6fcbb2f3f579797705d9ebd981c19940
Romulo Rocha <romulorocha063@gmail.com>
2019-09-09 20:05:38 -0300

    Remove all console.logs

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,14 +22,11 @@ class Home extends Component {
       priceFormatted: formatPrice(product.price),
     }));
 
-    console.log('darta', response.data);
-
     this.setState({ products: data });
   }
 
   handleAddProduct = id => {
     const { addToCartRequest } = this.props;
-    console.log('id', id);
 
     addToCartRequest(id);
   };
@@ -37,7 +34,7 @@ class Home extends Component {
   render() {
     const { products } = this.state;
     const { amount } = this.props;
-    console.log(amount);
+
     return (
       <ProductList>
         {products.map(product => (
