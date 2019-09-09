d9a25aa3821e1313183ad41d2884d29551184fb7
Romulo Rocha <romulorocha063@gmail.com>
2019-09-09 19:03:59 -0300

    Conditional add by stock

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,12 +1,37 @@
-import { call, put, all, takeLatest } from 'redux-saga/effects';
+import { call, put, all, select, takeLatest } from 'redux-saga/effects';
 import api from '../../../services/api';
-import { addToCartSuccess } from './actions';
+import { addToCartSuccess, updateAmount } from './actions';
+import { formatPrice } from '../../../util/format';
 
 function* addToCart({ id }) {
-  console.log('addToCart id', id);
-  const response = yield call(api.get, `/products/${id}`);
-  console.log(response);
-  yield put(addToCartSuccess(response.data));
+  const productExists = yield select(state =>
+    state.cart.find(p => p.id === id)
+  );
+
+  const stock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExists ? productExists.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if (amount > stockAmount) {
+    console.tron.warn('ERRO');
+    return;
+  }
+
+  if (productExists) {
+    yield put(updateAmount(id, amount));
+  } else {
+    const response = yield call(api.get, `/products/${id}`);
+
+    const data = {
+      ...response.data,
+      amount: 1,
+      priceFormatted: formatPrice(response.data.price),
+    };
+    yield put(addToCartSuccess(data));
+  }
 }
 
 export default all([takeLatest('@cart/ADD_REQUEST', addToCart)]);
